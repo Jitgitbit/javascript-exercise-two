7d3c1b9ef0fbb7a5a242a1abdad2c8deea17a088
Jitgitbit <rowowolf@gmail.com>
2020-04-13 13:53:55 +0200

    randomSquare made

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ let score = document.querySelector('#score')
 
 let result = 0
 
-function randomSquare(){}
+function randomSquare(){
+  square.forEach(className => {
+    className.classList.remove('mole')
+  })
+  let randomPosition = square[Math.floor(Math.random()* 9)]
+  randomPosition.classList.add('mole')
+
+  hitPosition = randomPosition.id
+}
